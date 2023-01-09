28665dc9923cf1f955aefe0452a66822d8efa747
Nobelgrant <104269270+Nobelgrant@users.noreply.github.com>
2023-01-09 16:48:19 +0630

    Update web3.js

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -9,7 +9,7 @@ if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
 } else {
   // We are on the server *OR* the user is not running metamask
   const provider = new Web3.providers.HttpProvider(
-    https://data-seed-prebsc-1-s1.binance.org:8545"
+    https,//data-seed-prebsc-1-s1.binance.org:8545"
   );
   web3 = new Web3(provider);
 }
